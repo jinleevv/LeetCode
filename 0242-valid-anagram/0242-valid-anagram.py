6bf1633becaad66960e5fe8c39faff1046c03ea9
Jinwon Lee <89884098+Jin1L@users.noreply.github.com>
2023-08-31 22:32:54 -0400

    Time: 30 ms (83.08%), Space: 13.7 MB (87.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,5 +5,27 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+        count = {}
+
+        for x in s:
+            if x in count:
+                count[x] += 1
+            else:
+                count[x] = 1
+        
+        for x in t:
+            if x in count:
+                count[x] -= 1
+            else:
+                return False
+
+        for key, value in count.items():
+            if value != 0:
+                return False
+
+        return True
+
+        
+        
         
