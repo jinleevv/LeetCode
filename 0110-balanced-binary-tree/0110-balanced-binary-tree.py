2b471f9eef1993527436569e014837d70ddebed7
Jinwon Lee <89884098+Jin1L@users.noreply.github.com>
2023-09-02 01:47:45 -0400

    Time: 50 ms (85.18%), Space: 21.3 MB (20.55%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,16 +5,36 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def height(self, root):
-            if root is None:
-                return -1
-            return 1 + max(self.height(root.left), self.height(root.right))
+    # def height(self, root):
+    #         if root is None:
+    #             return -1
+    #         return 1 + max(self.height(root.left), self.height(root.right))
 
+    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    #     if root is None:
+    #         return True
+        
+    #     if abs(self.height(root.left) - self.height(root.right)) > 1:
+    #         return False
+        
+    #     return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def helper(self, root):
+        # An empty tree is balanced and has height -1
+        if not root:
+            return True, -1
+        
+        # Check subtrees to see if they are balanced. 
+        leftIsBalanced, leftHeight = self.helper(root.left)
+        if not leftIsBalanced:
+            return False, 0
+        rightIsBalanced, rightHeight = self.helper(root.right)
+        if not rightIsBalanced:
+            return False, 0
+        
+        # If the subtrees are balanced, check if the current tree is balanced
+        # using their height
+        return (abs(leftHeight - rightHeight) < 2), 1 + max(leftHeight, rightHeight)
+    
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
-            return True
-        
-        if abs(self.height(root.left) - self.height(root.right)) > 1:
-            return False
-        
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+        return self.helper(root)[0]
