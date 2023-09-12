22f26fff8181b1274925e69fb748d0023bba198c
Jinwon Lee <89884098+Jin1L@users.noreply.github.com>
2023-09-11 23:25:13 -0400

    Time: 44 ms (54.93%), Space: 16.1 MB (92.13%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,45 +1,24 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # store = {}
-        # first = True
-        # num = 0
-        # while n > 0:
-        #     if first:
-        #         stringify = str(n)
-        #         first = False
-        #     else:
-        #         stringify = str(num)
-        #         num = 0
-
-        #     for i in range(len(stringify)):
-        #         num += int(stringify[i]) ** 2  
-
-        #     if str(num) == "1":
-        #         return True
-
-        #     if num in store:
-        #         break
-        #     store[num] = num
-
-        # return False
-        used = []
-
-        while(n>0):
-            tmp = 0
-            while(n>0):
-                i = n%10
-
-                tmp += i*i
-                n = n//10
-
-            if(tmp in used):
-                return False
+        store = {}
+        first = True
+        num = 0
+        while n > 0:
+            if first:
+                stringify = str(n)
+                first = False
             else:
-                used.append(tmp)
+                stringify = str(num)
+                num = 0
 
-            if(tmp==1):
+            for i in range(len(stringify)):
+                num += int(stringify[i]) ** 2  
+
+            if str(num) == "1":
                 return True
-            
-            n = tmp
+
+            if num in store:
+                break
+            store[num] = num
 
         return False
