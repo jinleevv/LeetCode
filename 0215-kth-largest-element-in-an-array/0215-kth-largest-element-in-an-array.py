5c7a1743adb23ae49c667b7e3252bcd46ab1239f
Jinwon Lee <89884098+Jin1L@users.noreply.github.com>
2023-09-22 00:43:24 -0400

    Time: 530 ms (35.47%), Space: 30 MB (29.88%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -7,17 +7,24 @@ class Solution:
 #                 heappop(heap)
         
 #         return heap[0]
-        min_value = min(nums)
-        max_value = max(nums)
-        count = [0] * (max_value - min_value + 1)
+        def quick_select(nums, k):
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
 
-        for num in nums:
-            count[num - min_value] += 1
+            for num in nums:
+                if num > pivot:
+                    left.append(num)
+                elif num < pivot:
+                    right.append(num)
+                else:
+                    mid.append(num)
+            
+            if k <= len(left):
+                return quick_select(left, k)
+            
+            if len(left) + len(mid) < k:
+                return quick_select(right, k - len(left) - len(mid))
+            
+            return pivot
         
-        remain = k
-        for num in range(len(count) -1, -1, -1):
-            remain -= count[num]
-            if remain <= 0:
-                return num + min_value
-
-        return -1
+        return quick_select(nums, k)
