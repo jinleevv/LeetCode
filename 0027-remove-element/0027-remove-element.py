4ac3ad7ceeb0805a00c8dc49bd2ae78ccc1dfe2f
Jin Won Lee <jinwon.lee@mail.mcgill.ca>
2024-12-09 00:52:50 -0500

    Time: 0 ms (100.00%), Space: 17.2 MB (9.28%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,11 +1,7 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0
-
-        while i <= len(nums) - 1:
-            if nums[i] == val:
-                nums.pop(i)
-            else:
-                i += 1
-                
+        
+        while val in nums:
+            nums.remove(val)
+        
         return len(nums)
